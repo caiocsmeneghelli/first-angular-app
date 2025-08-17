89b881e5a634c2eb4059339abb902f727e8c6f42
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-08-17 11:22:37 -0300

    iniciando com signal.

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -7,16 +7,20 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-
-  onSelectUser(){
-    console.log("clicked");
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+  // using zone.js
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
