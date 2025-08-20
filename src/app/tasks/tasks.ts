172f0221be0c7c2cb348bedd4460389a827b5dc8
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-08-20 10:20:27 -0300

    add tasks

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,11 +1,43 @@
 import { Component, Input } from '@angular/core';
 
+import { Task } from './task/task';
+
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
+  @Input({required: true}) userId!: string;
   @Input() selectedUser: string | undefined;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Task 1',
+      summary: 'Description for Task 1',
+      dueDate: new Date('2023-03-15')
+    },
+    {
+      id: 't2',
+      userId: 'u1',
+      title: 'Task 2',
+      summary: 'Description for Task 2',
+      dueDate: new Date('2023-03-16')
+    },
+    {
+      id: 't3',
+      userId: 'u1',
+      title: 'Task 3',
+      summary: 'Description for Task 3',
+      dueDate: new Date('2023-03-17')
+    }
+  ];
+
+  get selectedUserTasks() {
+    console.log('Selected User ID:', this.userId);
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
